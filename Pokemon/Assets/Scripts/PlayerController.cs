54dc8dd839110ed9f8ae2c434cbc10f0abf3429d
Karen Davtyan <karendavtyan21@mail.com>
2025-03-14 11:27:39 -0400

    Added idle and walking animation to player.

--- Pokemon/Assets/Scripts/PlayerController.cs
+++ Pokemon/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,13 @@ public class PlayerController : MonoBehaviour
     public bool isMoving;
     private Vector2 input;
 
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +35,10 @@ public class PlayerController : MonoBehaviour
 
             if(input != Vector2.zero)
             {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
@@ -35,6 +46,8 @@ public class PlayerController : MonoBehaviour
                 StartCoroutine(Move(targetPos));
             }
         }
+
+        animator.SetBool("isMoving", isMoving);
     }
     IEnumerator Move(Vector3 targetPos)
     {
